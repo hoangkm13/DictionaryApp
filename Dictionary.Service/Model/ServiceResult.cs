80246cfe2d03fc951df0b40069664d37381517ad
hoangkm13 <hoang.workingemail@gmail.com>
2023-07-10 13:37:06 +0700

    refactor ServiceResult.cs

---
+++ Dictionary.Service/Model/ServiceResult.cs
@@ -0,0 +1,24 @@
+﻿namespace Dictionary.Service.Model;
+
+public class ServiceResult
+{
+    public ServiceResult(int status, string message, string code, dynamic data, int errorCode)
+    {
+        Status = status;
+        Message = message;
+        Code = code;
+        Data = data;
+        ErrorCode = errorCode;
+    }
+
+    public int Status { get; set; }
+
+
+    public string Message { get; set; }
+
+    public string Code { get; set; }
+
+    public dynamic Data { get; set; }
+
+    public int ErrorCode { get; set; }
+}
